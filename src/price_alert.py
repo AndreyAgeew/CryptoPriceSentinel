0b315218ed92d24bb89b2b55d26dd87759fa4d1b
Andrey Ageew <faceofdeviler@gmail.com>
2023-12-29 17:16:38 +0300

    Update check_price_movement

--- src/price_alert.py
+++ src/price_alert.py
@@ -7,20 +7,24 @@ def check_price_movement():
         try:
             cursor = conn.cursor()
             query = """
-            SELECT eth_price FROM price_data 
+            SELECT eth_price, btc_price FROM price_data 
             WHERE timestamp >= %s
             ORDER BY timestamp ASC
             """
             one_hour_ago = datetime.now() - timedelta(hours=1)
             cursor.execute(query, (one_hour_ago,))
-            prices = cursor.fetchall()
+            records = cursor.fetchall()
 
-            if prices:
-                start_price = prices[0][0]
-                current_price = prices[-1][0]
+            if records:
+                start_eth_price, start_btc_price = records[0]
+                end_eth_price, end_btc_price = records[-1]
 
-                if abs((current_price - start_price) / start_price) >= 0.01:
-                    print(f"ETH price moved by 1% or more in the last 60 minutes: {current_price}")
+                eth_change = (end_eth_price - start_eth_price) / start_eth_price
+                btc_change = (end_btc_price - start_btc_price) / start_btc_price
+
+                # Проверяем изменение цены ETH по отношению к BTC
+                if abs(eth_change - btc_change) >= 0.01:
+                    print(f"Significant independent movement in ETH price detected: {end_eth_price}")
         except Exception as e:
             print(f"Error checking price movement: {e}")
         finally:
